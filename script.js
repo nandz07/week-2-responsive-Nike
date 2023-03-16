ac19dc25dd241c015fba016df8b7f5e2088e17fb
Nandakumar T <nandakumart07@gmail.com>
2023-03-16 19:35:50 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+//let menu=document.getElementById("menu").style.display="none"
+
+
+
+let newFeature=document.getElementById("newFeature")
+
+console.log(newFeature)
+newFeature.addEventListener("mouseover",()=>{
+    let menu=document.getElementById("menu")
+    menu.style.display="block"
+})
+newFeature.addEventListener("mouseout",()=>{
+    let menu=document.getElementById("menu")
+    menu.style.display="none"
+})
+
